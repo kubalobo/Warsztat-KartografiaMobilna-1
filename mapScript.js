7801c9319d2ea0a9d6a5b655dc1a9a919a7c0fa2
Kuba <kubalobo@gmail.com>
2021-06-16 20:52:01 +0200

    GPX support

--- mapScript.js
+++ mapScript.js
@@ -1,4 +1,4 @@
-const mymap = L.map('mapid').setView([52.230701, 21.005249], 25);
+const mymap = L.map('mapid').setView([51.9835167, 20.9513169], 22);
 
 // const Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
 // 	attribution: '',
@@ -18,4 +18,13 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoia3ViYWxvYm8iLCJhIjoiY2tlYTM3ZzBxMjJ6dzJzbDY4MGo0Z3E2ZSJ9.bUY1hv2zymhH_-HU0p84-A'
+}).addTo(mymap);
+
+
+
+
+var gpx = './warsztat1.gpx'; // URL to your GPX file or the GPX itself
+
+new L.GPX(gpx, {async: true}).on('loaded', function(e) {
+    mymap.fitBounds(e.target.getBounds());
 }).addTo(mymap);
