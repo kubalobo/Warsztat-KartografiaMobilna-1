3ecde1929db2dac36f2db4a58228f275219b1139
Kuba <kubalobo@gmail.com>
2021-06-16 18:25:18 +0200

    Akcja przycisku

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const imie = "Zosia";
+let zwierze = 'kota';
+console.log(imie + " ma " + zwierze + "... 🐈🐈🐈");
+
+
+function showEmail() {
+    console.log("😎😎😎");
+
+    const contactSpace = document.querySelector('#contactSpace');
+
+    contactSpace.innerHTML = '<a href="mailto:rozpruwacz@buziaczek.pl">rozpruwacz@buziaczek.pl</a>'
+
+}
